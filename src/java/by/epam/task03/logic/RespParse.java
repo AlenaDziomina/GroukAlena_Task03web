af8d6569523508ade20c086a62ac41d2ef1fe2d1
alena <alena@192.168.1.6>
2014-08-21 19:17:09 +0300

    Merge origin/equip into local
    Conflicts:
    	build.xml
    	manifest.mf
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	src/task03/Task03.java

--- src/java/by/epam/task03/logic/RespParse.java
+++ src/java/by/epam/task03/logic/RespParse.java
@@ -10,14 +10,7 @@ import by.epam.task03.entity.MotoEquipment;
 import by.epam.task03.exeption.NullInitException;
 import by.epam.task03.exeption.ValidatingException;
 import static by.epam.task03.logic.EquipBuilderFactory.createStudentBuilder;
-import java.io.File;
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.nio.charset.Charset;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -38,6 +31,7 @@ public class RespParse {
 
     public void getResponse() throws NullInitException, ValidatingException {
         
+        
         request.removeAttribute("errors");
        
         String strParser = request.getParameter("parser");
@@ -49,14 +43,14 @@ public class RespParse {
         String str = System.getProperty("user.home");
         String path = str+"/NetBeansProjects/Task03web/";
         
-        URL pathURL = AbstractEquipBuilder.class.getProtectionDomain().getCodeSource().getLocation();
-        try {
-            String classFile = URLDecoder.decode(pathURL.getFile().substring(1).replace('/', File.separatorChar),
-                    Charset.defaultCharset().name());
-            String propertiesFile = classFile.replace(".jar", ".properties");
-        } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(RespParse.class.getName()).log(Level.SEVERE, null, ex);
-        }
+//        URL pathURL = AbstractEquipBuilder.class.getProtectionDomain().getCodeSource().getLocation();
+//        try {
+//            String classFile = URLDecoder.decode(pathURL.getFile().substring(1).replace('/', File.separatorChar),
+//                    Charset.defaultCharset().name());
+//            String propertiesFile = classFile.replace(".jar", ".properties");
+//        } catch (UnsupportedEncodingException ex) {
+//            Logger.getLogger(RespParse.class.getName()).log(Level.SEVERE, null, ex);
+//        }
         
         
         AbstractEquipBuilder builder;
