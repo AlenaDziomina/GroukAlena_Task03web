11ea369d6e27efcc9ada7324c3f3391a3fef84c7
alena <alena@192.168.1.6>
2014-09-04 01:14:45 +0300

    Merge origin/equip into local
    Conflicts:
    	build.xml
    	manifest.mf
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	src/task03/Task03.java

--- src/java/by/epam/task03web/controller/ParsServlet.java
+++ src/java/by/epam/task03web/controller/ParsServlet.java
@@ -92,7 +92,6 @@ public class ParsServlet extends HttpServlet {
         
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -129,6 +128,6 @@ public class ParsServlet extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
